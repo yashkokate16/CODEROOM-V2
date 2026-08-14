ebdb22d4e5984ccd7fd4585fc70e03ff6eb206d7
Yash Kokate <yashkokate2000@gmail.com>
2026-08-14 16:16:37 +0530

    making fronetend production ready

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppRoutes from './app/routes/AppRoutes.jsx'
+import AppRoutes from "./app/routes/AppRoutes.jsx"
 import { Provider } from 'react-redux'
 import {store} from "./app/config/store.jsx"
 
