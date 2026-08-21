c8c11bfdea1397f12bcb0edde96e30dbaa35ff7e
yash <yashkokate2000@gmail.com>
2026-08-21 16:25:03 +0530

    fix AppRoute

--- client/src/app/routes/AppRoutes.jsx
+++ client/src/app/routes/AppRoutes.jsx
@@ -65,7 +65,7 @@ const AppRoutes = () => {
                     element:<Home />
                 },
                 {
-                    path:"/home/profile",
+                    path:"profile",
                     element:<Profile />,
                 }
             ]
