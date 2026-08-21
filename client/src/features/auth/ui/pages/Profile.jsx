e06b23517d747014890490450ad67b63f89d6b1c
yash <yashkokate2000@gmail.com>
2026-08-21 16:52:11 +0530

    fix bugs

--- client/src/features/auth/ui/pages/Profile.jsx
+++ client/src/features/auth/ui/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 const Profile = () => {
 
-     const { user } = useSelector((state) => state.auth);
+     const { user, isLoading } = useSelector((state) => state.auth);
     let User = user.user
 
     if (isLoading) {
